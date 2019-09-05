3732ce1509ae0167e587fb014bdfb219cd9a0ee9
Xiaoqiang1018 <yulongfox@gmail.com>
2019-09-05 17:36:16 +0800

    jwt

---
+++ demo-jwt/src/main/java/com/jwt/demojwt/util/UserSalt.java
@@ -0,0 +1,15 @@
+package com.jwt.demojwt.util;
+
+/* *
+ * @ProjectName:    demo-jwt
+ * @Package:        com.jwt.demojwt.util
+ * @ClassName:      UserSalt
+ * @Author:     xiaoqiang
+ * @Description:  ${description}
+ * @Date:    2019/9/5
+ * @Version:    1.0
+ */
+public interface UserSalt {
+
+    static final String USER_LOGIN_SALT = "elitel!@3$";
+}
